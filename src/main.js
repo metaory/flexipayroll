bbad05ef14990252d2af0ab60c1eaee047596f9a
metaory <metaory@gmail.com>
2025-07-04 00:14:33 +0330

    0704_0014

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
- 
+import "@fontsource/bungee"
+import "@fontsource/baloo-2"
+import './styles.sass'
+
+import { mount } from 'svelte'
+import App from './App.svelte'
+
+mount(App, { target: document.getElementById('app') }) 
